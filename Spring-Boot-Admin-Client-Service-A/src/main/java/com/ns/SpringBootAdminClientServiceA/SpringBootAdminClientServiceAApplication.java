5012e9be66aafc5968f37a280dd97cf310c59a40
nirajsonawane <niraj.sonawane@gmail.com>
2019-06-08 10:20:11 +0530

    Spring Admin Setup

---
+++ Spring-Boot-Admin-Client-Service-A/src/main/java/com/ns/SpringBootAdminClientServiceA/SpringBootAdminClientServiceAApplication.java
@@ -0,0 +1,13 @@
+package com.ns.SpringBootAdminClientServiceA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminClientServiceAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminClientServiceAApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-Admin-Client-Service-A/src/test/java/com/ns/SpringBootAdminClientServiceA/SpringBootAdminClientServiceAApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ns.SpringBootAdminClientServiceA;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAdminClientServiceAApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
